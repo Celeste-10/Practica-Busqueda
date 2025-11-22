2df93c32491a6040e6159876826f6436e1388b33
Maria Celeste Carrasco Obando <mccarrasco@uamv.edu.ni>
2025-11-22 14:52:11 -0600

    Se realizo el ejercicio 2

--- Practica/Ejercicios/Ejercicio2.Designer.cs
+++ Practica/Ejercicios/Ejercicio2.Designer.cs
@@ -28,19 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lstLista = new System.Windows.Forms.ListBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.lstProceso = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
+            // 
+            // lstLista
+            // 
+            this.lstLista.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstLista.FormattingEnabled = true;
+            this.lstLista.ItemHeight = 20;
+            this.lstLista.Location = new System.Drawing.Point(46, 34);
+            this.lstLista.Name = "lstLista";
+            this.lstLista.Size = new System.Drawing.Size(310, 224);
+            this.lstLista.TabIndex = 0;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.Location = new System.Drawing.Point(484, 94);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(197, 84);
+            this.btnGenerar.TabIndex = 1;
+            this.btnGenerar.Text = "Generar arreglo";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(9, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Ingrese un número:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.Location = new System.Drawing.Point(199, 320);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(285, 27);
+            this.txtNumero.TabIndex = 3;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(553, 305);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(128, 56);
+            this.btnBuscar.TabIndex = 4;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // lstProceso
+            // 
+            this.lstProceso.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstProceso.FormattingEnabled = true;
+            this.lstProceso.ItemHeight = 20;
+            this.lstProceso.Location = new System.Drawing.Point(174, 407);
+            this.lstProceso.Name = "lstProceso";
+            this.lstProceso.Size = new System.Drawing.Size(329, 244);
+            this.lstProceso.TabIndex = 5;
             // 
             // Ejercicio2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 498);
+            this.ClientSize = new System.Drawing.Size(758, 707);
+            this.Controls.Add(this.lstProceso);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.lstLista);
             this.Name = "Ejercicio2";
             this.Text = "Ejercicio2";
+            this.Load += new System.EventHandler(this.Ejercicio2_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstLista;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.ListBox lstProceso;
     }
 }
--- Practica/Ejercicios/Ejercicio2.cs
+++ Practica/Ejercicios/Ejercicio2.cs
@@ -12,9 +12,59 @@ namespace Practica.Ejercicios
 {
     public partial class Ejercicio2 : Form
     {
+        List<int> lista = new List<int>();
         public Ejercicio2()
         {
             InitializeComponent();
+        }
+
+        private void Ejercicio2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            lista.Clear();
+            for (int i = 0; i < 30; i++)
+                lista.Add(r.Next(1, 100));
+
+            lista.Sort(); 
+
+            lstLista.Items.Clear();
+            foreach (var n in lista)
+                lstLista.Items.Add(n);
+
+            lstProceso.Items.Clear();
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            int numero = int.Parse(txtNumero.Text);
+            int inicio = 0;
+            int fin = lista.Count - 1;
+
+            lstProceso.Items.Clear();
+
+            while (inicio <= fin)
+            {
+                int mitad = (inicio + fin) / 2;
+                lstProceso.Items.Add($"Mitad: {mitad} (valor: {lista[mitad]})");
+
+                if (lista[mitad] == numero)
+                {
+                    lstProceso.Items.Add("Número encontrado.");
+                    return;
+                }
+
+                if (numero < lista[mitad])
+                    fin = mitad - 1;
+                else
+                    inicio = mitad + 1;
+            }
+
+            lstProceso.Items.Add("Número NO encontrado.");
         }
     }
 }
