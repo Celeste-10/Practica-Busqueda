e3bd0343c0fcfc4b0b0955f10af6091c2f4be4eb
Maria Celeste Carrasco Obando <mccarrasco@uamv.edu.ni>
2025-11-23 13:44:25 -0600

    Ejercicio 5 finalizado

--- Practica/Ejercicios/Ejercicio5.Designer.cs
+++ Practica/Ejercicios/Ejercicio5.Designer.cs
@@ -28,20 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lstLista = new System.Windows.Forms.ListBox();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.lblIteraciones = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.Location = new System.Drawing.Point(457, 82);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(167, 62);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(457, 201);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(167, 62);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lstLista
+            // 
+            this.lstLista.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstLista.FormattingEnabled = true;
+            this.lstLista.ItemHeight = 20;
+            this.lstLista.Location = new System.Drawing.Point(51, 53);
+            this.lstLista.Name = "lstLista";
+            this.lstLista.Size = new System.Drawing.Size(283, 244);
+            this.lstLista.TabIndex = 2;
+            // 
+            // lblMax
+            // 
+            this.lblMax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMax.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMax.Location = new System.Drawing.Point(39, 364);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(218, 77);
+            this.lblMax.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(374, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 4;
+            // 
+            // lblMin
+            // 
+            this.lblMin.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMin.Location = new System.Drawing.Point(291, 364);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(218, 77);
+            this.lblMin.TabIndex = 5;
+            // 
+            // lblIteraciones
+            // 
+            this.lblIteraciones.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblIteraciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIteraciones.Location = new System.Drawing.Point(541, 364);
+            this.lblIteraciones.Name = "lblIteraciones";
+            this.lblIteraciones.Size = new System.Drawing.Size(218, 77);
+            this.lblIteraciones.TabIndex = 6;
+            this.lblIteraciones.Click += new System.EventHandler(this.lblIteraciones_Click);
             // 
             // Ejercicio5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 532);
+            this.Controls.Add(this.lblIteraciones);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.lstLista);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnGenerar);
             this.Name = "Ejercicio5";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ejercicio5";
+            this.Load += new System.EventHandler(this.Ejercicio5_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.ListBox lstLista;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.Label lblIteraciones;
     }
 }
--- Practica/Ejercicios/Ejercicio5.cs
+++ Practica/Ejercicios/Ejercicio5.cs
@@ -12,9 +12,51 @@ namespace Practica.Ejercicios
 {
     public partial class Ejercicio5 : Form
     {
+        List<int> lista = new List<int>();
         public Ejercicio5()
         {
             InitializeComponent();
+        }
+
+        private void Ejercicio5_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            lista.Clear();
+
+            for (int i = 0; i < 20; i++)
+                lista.Add(r.Next(1, 100));
+
+            lstLista.Items.Clear();
+            foreach (var n in lista)
+                lstLista.Items.Add(n);
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int max = lista[0];
+            int min = lista[0];
+            int iteraciones = 0;
+
+            foreach (var n in lista)
+            {
+                if (n > max) max = n;
+                if (n < min) min = n;
+                iteraciones++;
+            }
+
+            lblMax.Text = $"Max: {max}";
+            lblMin.Text = $"Min: {min}";
+            lblIteraciones.Text = $"Iteraciones: {iteraciones}";
+        }
+
+        private void lblIteraciones_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
