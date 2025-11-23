c2484f27d36c65f1d4f806bf1fb74c3d1f8be508
Maria Celeste Carrasco Obando <mccarrasco@uamv.edu.ni>
2025-11-23 13:57:16 -0600

    Ejercicio 6 finalizado

--- Practica/Ejercicios/Ejercicio6.Designer.cs
+++ Practica/Ejercicios/Ejercicio6.Designer.cs
@@ -28,20 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dgvMatriz = new System.Windows.Forms.DataGridView();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dgvMatriz
+            // 
+            this.dgvMatriz.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMatriz.Location = new System.Drawing.Point(45, 47);
+            this.dgvMatriz.Name = "dgvMatriz";
+            this.dgvMatriz.RowHeadersWidth = 51;
+            this.dgvMatriz.RowTemplate.Height = 24;
+            this.dgvMatriz.Size = new System.Drawing.Size(404, 361);
+            this.dgvMatriz.TabIndex = 0;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGenerar.Location = new System.Drawing.Point(485, 178);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(188, 86);
+            this.btnGenerar.TabIndex = 1;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(457, 436);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(151, 53);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 452);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Numero:";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.Location = new System.Drawing.Point(153, 452);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(248, 27);
+            this.txtNumero.TabIndex = 4;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(113, 540);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(378, 81);
+            this.lblResultado.TabIndex = 5;
             // 
             // Ejercicio6
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(731, 711);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.dgvMatriz);
             this.Name = "Ejercicio6";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ejercicio6";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMatriz)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dgvMatriz;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Label lblResultado;
     }
 }
--- Practica/Ejercicios/Ejercicio6.cs
+++ Practica/Ejercicios/Ejercicio6.cs
@@ -12,9 +12,58 @@ namespace Practica.Ejercicios
 {
     public partial class Ejercicio6 : Form
     {
+        int[,] matriz = new int[10, 10];
         public Ejercicio6()
         {
             InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+
+            dgvMatriz.Columns.Clear();
+            dgvMatriz.Rows.Clear();
+
+            // Crear 10 columnas
+            for (int c = 0; c < 10; c++)
+            {
+                dgvMatriz.Columns.Add($"C{c}", $"C{c}");
+                dgvMatriz.Columns[c].Width = 40; // para que no se vea grande
+            }
+
+            // Crear 10 filas
+            dgvMatriz.Rows.Add(10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    matriz[i, j] = r.Next(1, 100);
+                    dgvMatriz.Rows[i].Cells[j].Value = matriz[i, j];
+                }
+            }
+            MessageBox.Show("Matriz 10x10 generada correctamente");
+            
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            int numero = int.Parse(txtNumero.Text);
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    if (matriz[i, j] == numero)
+                    {
+                        lblResultado.Text = $"Encontrado en fila {i}, columna {j}.";
+                        return;
+                    }
+                }
+            }
+
+            lblResultado.Text = "Número NO encontrado.";
         }
     }
 }
