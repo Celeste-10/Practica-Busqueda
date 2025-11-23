5827952ed876c9a4fad19fa27ef5c9e5f7899fbc
Maria Celeste Carrasco Obando <mccarrasco@uamv.edu.ni>
2025-11-23 13:23:43 -0600

    Ejercicio 4 terminado

--- Practica/Ejercicios/Ejercicio4.Designer.cs
+++ Practica/Ejercicios/Ejercicio4.Designer.cs
@@ -28,20 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnBuscarId = new System.Windows.Forms.Button();
+            this.btnBuscarNombre = new System.Windows.Forms.Button();
+            this.lblResultadoId = new System.Windows.Forms.Label();
+            this.lblResultadoNombre = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 250);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(122, 67);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(319, 27);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtId.Location = new System.Drawing.Point(88, 250);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(208, 27);
+            this.txtId.TabIndex = 3;
+            // 
+            // btnBuscarId
+            // 
+            this.btnBuscarId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscarId.Location = new System.Drawing.Point(376, 232);
+            this.btnBuscarId.Name = "btnBuscarId";
+            this.btnBuscarId.Size = new System.Drawing.Size(202, 62);
+            this.btnBuscarId.TabIndex = 4;
+            this.btnBuscarId.Text = "Buscar ID";
+            this.btnBuscarId.UseVisualStyleBackColor = true;
+            this.btnBuscarId.Click += new System.EventHandler(this.btnBuscarId_Click);
+            // 
+            // btnBuscarNombre
+            // 
+            this.btnBuscarNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscarNombre.Location = new System.Drawing.Point(504, 53);
+            this.btnBuscarNombre.Name = "btnBuscarNombre";
+            this.btnBuscarNombre.Size = new System.Drawing.Size(202, 62);
+            this.btnBuscarNombre.TabIndex = 5;
+            this.btnBuscarNombre.Text = "Buscar Nombre";
+            this.btnBuscarNombre.UseVisualStyleBackColor = true;
+            this.btnBuscarNombre.Click += new System.EventHandler(this.btnBuscarNombre_Click);
+            // 
+            // lblResultadoId
+            // 
+            this.lblResultadoId.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultadoId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultadoId.Location = new System.Drawing.Point(191, 319);
+            this.lblResultadoId.Name = "lblResultadoId";
+            this.lblResultadoId.Size = new System.Drawing.Size(317, 83);
+            this.lblResultadoId.TabIndex = 7;
+            // 
+            // lblResultadoNombre
+            // 
+            this.lblResultadoNombre.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblResultadoNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultadoNombre.Location = new System.Drawing.Point(191, 143);
+            this.lblResultadoNombre.Name = "lblResultadoNombre";
+            this.lblResultadoNombre.Size = new System.Drawing.Size(317, 69);
+            this.lblResultadoNombre.TabIndex = 8;
             // 
             // Ejercicio4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(754, 482);
+            this.Controls.Add(this.lblResultadoNombre);
+            this.Controls.Add(this.lblResultadoId);
+            this.Controls.Add(this.btnBuscarNombre);
+            this.Controls.Add(this.btnBuscarId);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Ejercicio4";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ejercicio4";
+            this.Load += new System.EventHandler(this.Ejercicio4_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnBuscarId;
+        private System.Windows.Forms.Button btnBuscarNombre;
+        private System.Windows.Forms.Label lblResultadoId;
+        private System.Windows.Forms.Label lblResultadoNombre;
     }
 }
--- Practica/Ejercicios/Ejercicio4.cs
+++ Practica/Ejercicios/Ejercicio4.cs
@@ -12,9 +12,105 @@ namespace Practica.Ejercicios
 {
     public partial class Ejercicio4 : Form
     {
+        List<Estudiante> estudiantes;
         public Ejercicio4()
         {
             InitializeComponent();
+            CargarEstudiantes();
+        }
+        private void CargarEstudiantes()
+        {
+            // Lista con 10 estudiantes
+            estudiantes = new List<Estudiante>()
+            {
+                new Estudiante { Id = 1, Nombre = "Ana" },
+                new Estudiante { Id = 2, Nombre = "Brenda" },
+                new Estudiante { Id = 3, Nombre = "Carlos" },
+                new Estudiante { Id = 4, Nombre = "Daniel" },
+                new Estudiante { Id = 5, Nombre = "Elena" },
+                new Estudiante { Id = 6, Nombre = "Fernanda" },
+                new Estudiante { Id = 7, Nombre = "Gabriel" },
+                new Estudiante { Id = 8, Nombre = "Hector" },
+                new Estudiante { Id = 9, Nombre = "Iván" },
+                new Estudiante { Id = 10, Nombre = "Julia" },
+            };
+        }
+
+        private void Ejercicio4_Load(object sender, EventArgs e)
+        {
+            
+        }
+        private Estudiante BusquedaLineal(int id)
+        {
+            foreach (var est in estudiantes)
+            {
+                if (est.Id == id)
+                    return est;
+            }
+            return null;
+        }
+        private void btnBuscarId_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtId.Text, out int idBuscado))
+            {
+                lblResultadoId.Text = "Ingrese un ID válido.";
+                return;
+            }
+
+            Estudiante encontrado = BusquedaLineal(idBuscado);
+
+            if (encontrado != null)
+                lblResultadoId.Text = $"Encontrado: {encontrado.Id} - {encontrado.Nombre}";
+            else
+                lblResultadoId.Text = "No se encontró el estudiante.";
+        }
+
+        private void btnBuscarNombre_Click(object sender, EventArgs e)
+        {
+            string nombreBuscado = txtNombre.Text.Trim();
+
+            if (string.IsNullOrEmpty(nombreBuscado))
+            {
+                lblResultadoNombre.Text = "Ingrese un nombre.";
+                return;
+            }
+
+            var listaOrdenada = estudiantes.OrderBy(x => x.Nombre).ToList();
+
+            Estudiante encontrado = BusquedaBinaria(listaOrdenada, nombreBuscado);
+
+            if (encontrado != null)
+                lblResultadoNombre.Text = $"Encontrado: {encontrado.Id} - {encontrado.Nombre}";
+            else
+                lblResultadoNombre.Text = "No se encontró el estudiante.";
+        }
+
+        private Estudiante BusquedaBinaria(List<Estudiante> lista, string nombre)
+        {
+            int inicio = 0;
+            int fin = lista.Count - 1;
+
+            while (inicio <= fin)
+            {
+                int medio = (inicio + fin) / 2;
+
+                int comparacion = string.Compare(lista[medio].Nombre, nombre, StringComparison.OrdinalIgnoreCase);
+
+                if (comparacion == 0)
+                {
+                    return lista[medio];
+                }
+                else if (comparacion < 0)
+                {
+                    inicio = medio + 1;
+                }
+                else
+                {
+                    fin = medio - 1;
+                }
+            }
+
+            return null;
         }
     }
 }
